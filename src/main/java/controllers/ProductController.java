53671d04a6f72a4cb8ffb1fc81d5d3eb2bee01b5
GreatRacc <EvilGrea@yandex.ru>
2024-12-20 14:06:29 +0400

    Product +- Ready

--- src/main/java/controllers/ProductController.java
+++ src/main/java/controllers/ProductController.java
@@ -1,4 +1,70 @@
 package controllers;
 
+import models.ProductModel;
+import services.ProductService;
+
+import java.util.List;
+import java.util.Scanner;
+
 public class ProductController {
+    private final ProductService productService;
+
+    public ProductController(ProductService productService) {
+        this.productService = productService;
+    }
+
+    public void showMenu() {
+        Scanner scanner = new Scanner(System.in);
+        while (true) {
+            System.out.println("===== Управление товарами =====");
+            System.out.println("1. Добавить товар");
+            System.out.println("2. Показать все товары");
+            System.out.println("0. Назад");
+            System.out.print("Выберите действие: ");
+            int choice = scanner.nextInt();
+            if (choice == 0) break;
+
+            switch (choice) {
+                case 1:
+                    addProduct();
+                    break;
+                case 2:
+                    showAllProducts();
+                    break;
+                default:
+                    System.out.println("Некорректный выбор. Попробуйте еще раз.");
+            }
+        }
+    }
+
+    private void showAllProducts() {
+        List<ProductModel> products = productService.getAllProducts();
+        products.forEach(System.out::println);
+    }
+
+    private void addProduct() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Введите название товара: ");
+        String name = scanner.nextLine();
+        System.out.print("Введите цену товара: ");
+        double price = scanner.nextDouble();
+        scanner.nextLine();
+        System.out.print("Введите категорию товара: FOOD, ELECTRONICS, CLOTHING. ");
+        String category = scanner.nextLine().toUpperCase();
+        if (!isValidCategory(category)) {
+            System.out.println("Некорректная категория. Выберите что-то из: FOOD, ELECTRONICS, CLOTHING.");
+            return;
+        }
+        ProductModel product = new ProductModel(generateProductId(), name, price, category);
+        productService.addProduct(product);
+        System.out.println("Товар добавлен: " + product);
+    }
+
+    private int generateProductId() {
+        return productService.getAllProducts().size() + 1;
+    }
+
+    private boolean isValidCategory(String category) {
+        return category.equals("FOOD") || category.equals("ELECTRONICS") || category.equals("CLOTHING");
+    }
 }
--- src/main/java/services/ProductService.java
+++ src/main/java/services/ProductService.java
@@ -1,4 +1,26 @@
 package services;
 
+import models.ProductModel;
+import repositories.ProductRepository;
+
+import java.util.List;
+
 public class ProductService {
+    private final ProductRepository productRepository;
+
+    public ProductService(ProductRepository productRepository) {
+        this.productRepository = productRepository;
+    }
+
+    public void addProduct(ProductModel product) {
+        productRepository.save(product);
+    }
+
+    public ProductModel getProductById(int id) {
+        return productRepository.findById(id);
+    }
+
+    public List<ProductModel> getAllProducts() {
+        return productRepository.loadAll();
+    }
 }
