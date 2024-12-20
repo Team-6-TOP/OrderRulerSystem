28ad119b7f1e41456981874af7a476b9729052ee
JackieWeckie <nekit4568i@gmail.com>
2024-12-20 19:33:08 +0300

    Feature by Nikita Morozov

---
+++ src/main/java/services/OrderService/OrderConstants.java
@@ -0,0 +1,8 @@
+package services.OrderService;
+
+public interface OrderConstants {
+    String NEW = "Заказ успешно создан! Скоро начнём собирать...";
+    String PROCESSING = "Везём ваш заказ!";
+    String COMPLETED = "Ваш заказ доставлен! Спасибо, что выбрали нас!";
+    String CANCELLED = "Вы отменили заказ.";
+}
