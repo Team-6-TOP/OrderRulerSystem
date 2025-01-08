8b8419fe4ae6b03e8a878dc07c8fc6c17a7013c0
GreatRacc <EvilGrea@yandex.ru>
2025-01-08 20:04:59 +0400

    Переделал главный контроллер

--- src/main/java/controllers/MainController.java
+++ src/main/java/controllers/MainController.java
@@ -1,8 +1,12 @@
 package controllers;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import java.util.Scanner;
 
 public class MainController {
+    private static final Logger logger = LoggerFactory.getLogger(MainController.class);
     private final ProductController productController;
 
     public MainController(ProductController productController) {
@@ -12,20 +16,27 @@ public class MainController {
     public void run() {
         Scanner scanner = new Scanner(System.in);
         while (true) {
-            System.out.println("===== Главное меню =====");
-            System.out.println("1. Управление товарами");
-            System.out.println("2. Управление покупателями");
-            System.out.println("3. Управление заказами");
-            System.out.println("0. Выйти");
-            System.out.print("Выберите действие: ");
+            logger.info("===== Главное меню =====");
+            logger.info("1. Управление товарами");
+            logger.info("2. Управление покупателями");
+            logger.info("3. Управление заказами");
+            logger.info("0. Выйти");
+            logger.info("Выберите действие: ");
             int choice = scanner.nextInt();
-            if (choice == 0) break;
+            if (choice == 0) {
+                logger.info("Выход из приложения.");
+                break;
+            }
             switch (choice) {
-                case 1 -> productController.showMenu();
-                case 2 -> System.out.println("В разработке");
-                case 3 -> System.out.println("В разработке х2");
-                default -> System.out.println("Некорректный выбор. Попробуйте еще раз.");
+                case 1 -> {
+                    logger.info("Переход к меню управления товарами.");
+                    productController.showMenu();
+                }
+                case 2 -> logger.warn("в разработке.");
+                case 3 -> logger.warn("в разработке 2х.");
+                default -> logger.error("Некорректный выбор. Попробуйте еще раз.");
             }
         }
+        scanner.close();
     }
 }
